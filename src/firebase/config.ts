bc8f2add27aa118262b951a4ad0f59dbe50eb9ed
kehinde Olusegun <olusegunkehindejones@gmail.com>
2025-11-28 20:37:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5858385829-76c34",
+  "appId": "1:811878341850:web:0724ac6da9b3d18b7f59e3",
+  "apiKey": "AIzaSyBq4MePj7zDUKh7NTEd8B8hxNjsG2ePy8g",
+  "authDomain": "studio-5858385829-76c34.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "811878341850"
+};
